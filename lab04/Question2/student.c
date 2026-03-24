04029f194999866a3799b5779a46572c819349ed
Logan-R-TTU <loganrule05@gmail.com>
2026-03-24 16:01:20 -0500

    Update student.c

--- lab04/Question2/student.c
+++ lab04/Question2/student.c
@@ -52,4 +52,12 @@ struct TreeNode {
 
 bool isSymmetric(struct TreeNode* root) {
   // TODO: implement
+        if (root == NULL) return true;
+        return isMirror(root->left, root->right);
+}
+
+bool isMirror(struct TreeNode* p, struct TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q || p->val != q->val) return false;
+        return isMirror(p->left, q->right) && isMirror(p->right, q->left);
 }
